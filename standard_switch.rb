fb88971ecde78324cb78c931d4a7dcb9218dda81
misson20000 <xenotoad@xenotoad.net>
2017-04-17 21:00:52 -0700

    change SVC names to switchbrew.org names, add their SVCs

--- standard_switch.rb
+++ standard_switch.rb
@@ -13,6 +13,19 @@ module Types
   end
   PageInfo = StructType.new("PageInfo") do
     field Types::Uint64, :pageFlags
+    # lower 8 bits:
+    #  0x3: code static .text and .rodata
+    #  0x4: code .data
+    #  0x5: heap
+    #  0x6: shared memory block
+    #  0x8: module code static .text and .rodata
+    #  0x9: module code
+    #  0xB: stack mirror
+    #  0xC: thread local storage
+    #  0xE: memory mirror
+    #  0x10: reserved
+    # bit32: is_mirrored
+    # bit32: is_uncached?
   end
 end
 
@@ -98,17 +111,27 @@ module SVC
     0x52 => $dsl.mref(0x3BC000)
   }
 
+  # untested
   # can only be invoked once according to PegaSwitch guys, and it's already been blown by the time our code runs
-  CreateMemoryHeap = svc[0x01].bridge(Types::Result, Types::Handle.pointer, Types::Uint64).set_names("handle", "size")
-
-  # can change RW -> R
-  SetMemoryPermission = svc[0x02].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::Uint64).set_names("???", "size", "permission")
+  # size must be multiple of 0x2000000
+  SetHeapSize = svc[0x01].bridge(Types::Result, Types::Handle.pointer, Types::Uint64).set_names("handle", "size")
 
   # untested
-  MapMemory_maybe = svc[0x04].bridge(Types::Result, Types::Uint64, Types::Uint64, Types::Uint64)
+  # bit2 of permission (exec) is not allowed
+  # setting write-only also not allowed
+  ProtectMemory = svc[0x02].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::Uint64).set_names("addr", "size", "permission")
 
   # untested
-  UnmapMemory_maybe = svc[0x05].bridge(Types::Result, Types::Uint64, Types::Uint64, Types::Uint64)
+  SetMemoryState = svc[0x03].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::Uint64, Types::Uint64).set_names("addr", "size", "state0", "state1")
+  
+  # untested
+  # may only be mapped into special region
+  # range of this region can be found with SVC::GetInfo
+  # source region is reprotected to ---, bit32 in MemoryState is set
+  MirrorStack = svc[0x04].bridge(Types::Result, Types::Void.pointer, Types::Void.pointer, Types::Uint64).set_names("dst", "src", "size")
+
+  # untested
+  UnirrorStack = svc[0x05].bridge(Types::Result, Types::Void.pointer, Types::Void.pointer, Types::Uint64).set_names("dst", "src", "size")
 
   # working
   QueryMemory = svc[0x06].bridge(Types::Result, Types::MemInfo.pointer, Types::PageInfo.pointer, Types::Void.pointer).set_names("memoryInfo", "pageInfo", "addr")
@@ -117,9 +140,11 @@ module SVC
   ExitProcess = svc[0x07].bridge(Types::Void)
 
   # untested
-  CreateThread = svc[0x08].bridge(Types::Void, Types::Handle.pointer, Types::Void.pointer, Types::Uint64, Types::Void.pointer, Types::Int32, Types::Int32).set_names("out", "entrypoint", "arg", "stackTop", "priority", "coreNumber")
+  # processorNumber must be 0, 1, 2, 3, or -2
+  CreateThread = svc[0x08].bridge(Types::Void, Types::Handle.pointer, Types::Void.pointer, Types::Uint64, Types::Void.pointer, Types::Int32, Types::Int32).set_names("out", "entrypoint", "arg", "stackTop", "priority", "processorNumber")
 
   # untested
+  # nano = 0 means yield thread
   StartThread = svc[0x09].bridge(Types::Void, Types::Handle).set_names("thread")
 
   # untested
@@ -128,26 +153,96 @@ module SVC
   # working
   SleepThread = svc[0x0B].bridge(Types::Void, Types::Int64).set_names("nanoseconds")
 
+  # untested, args may be wrong (I haven't looked at the wrapper)
+  SetThreadPriority = svc[0x0D].bridge(Types::Result, Types::Handle.pointer, Types::Uint64).set_names("thread", "priority")
+  
   # working
-  GetCurrentCoreNumber = svc[0x10].bridge(Types::Int32)
-
-  # broken - no perms?
-  CreateMemoryBlock = svc[0x50].bridge(Types::Result, Types::Handle.pointer, Types::Uint64, Types::Uint64, Types::Uint64).set_names("memBlock", "size", "myPerm", "otherPerm")
+  GetCurrentProcessorNumber = svc[0x10].bridge(Types::Int32)
 
   # no perms?
-  MapMemoryBlock = svc[0x13].bridge(Types::Result, Types::Handle, Types::Uint64, Types::Uint64, Types::Uint64).set_names("memBlock", "addr", "size", "perm")
+  MapMemoryBlock = svc[0x13].bridge(Types::Result, Types::Handle, Types::Void.pointer, Types::Uint64, Types::Uint64).set_names("memBlock", "addr", "size", "perm")
 
   # no perms?
-  UnmapMemoryBlock = svc[0x14].bridge(Types::Result, Types::Handle).set_names("memBlock")
+  UnmapMemoryBlock = svc[0x14].bridge(Types::Result, Types::Handle, Types::Void.pointer, Types::Uint64).set_names("memBlock", "addr", "size")
+
+  # tested
+  # reprotects source block with given perms, also sets bit32 in MemoryState
+  # executable bit permission not allowed
+  # closing returned handle clears bit32 in MemoryState
+  CreateMemoryMirror = svc[0x15].bridge(Types::Result, Types::Handle.pointer, Types::Void.pointer, Types::Uint64, Types::Uint64).set_names("mirror", "addr", "size", "perm")
 
   # untested
+  CloseHandle = svc[0x16].bridge(Types::Result, Types::Handle).set_names("handle")
+
+  # untested
+  ClearEvent = svc[0x17].bridge(Types::Result, Types::Handle).set_names("handle")
+
+  # untested, args checked
+  WaitEvents = svc[0x18].bridge(Types::Result, Types::Uint64.pointer, Types::Handle.pointer, Types::Uint64, Types::Uint64).set_names("handle_idx", "handles", "num_handles", "timeout")
+
+  # untested
+  SignalEvent = svc[0x19].bridge(Types::Result, Types::Handle).set_names("handle")
+
+  # untested
+  LockMutex = svc[0x1A].bridge(Types::Void, Types::Handle, Types::Void.pointer, Types::Handle).set_names("cur_thread_handle", "ptr", "req_thread_handle")
+
+  # untested
+  UnlockMutex = svc[0x1B].bridge(Types::Void, Types::Void.pointer).set_names("ptr")
+
+  # untested
+  CondWait = svc[0x1C].bridge(Types::Result, Types::Void.pointer, Types::Void.pointer, Types::Handle, Types::Uint64).set_names("ptr0", "ptr", "thread_handle", "timeout")
+
+  # untested
+  CondBroadcast = svc[0x1D].bridge(Types::Result, Types::Void.pointer, Types::Uint64).set_names("ptr", "value")
+  
+  # tested, works fine
   ConnectToPort = svc[0x1F].bridge(Types::Result, Types::SessionHandle.pointer, Types::Char.pointer).set_names("out", "portName")
 
   # untested
   SendSyncRequest = svc[0x21].bridge(Types::Result, Types::SessionHandle).set_names("session")
 
   # untested
-  SendSyncRequestEx = svc[0x22].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::SessionHandle).set_names("addr", "size", "session")
+  SendSyncRequestByBuf = svc[0x22].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::SessionHandle).set_names("cmdbufptr", "size", "session")
+
+  # untested
+  GetThreadId = svc[0x25].bridge(Types::Result, Types::Uint64.pointer, Types::Handle).set_names("out", "thread_handle")
+
+  # untested
+  # Break = svc[0x26].bridge( ? )
+
+  # untested
+  OutputDebugString = svc[0x27].bridge(Types::Void, Types::Char.pointer, Types::Uint64).set_names("str", "size")
+
+  # untested
+  ReturnFromException = svc[0x28].bridge(Types::Void, Types::Result).set_names("result")
+
+  # untested
+  GetInfo = svc[0x29].bridge(Types::Result, Types::Void.pointer, Types::Uint64, Types::Handle, Types::Uint64).set_names("out", "info_id", "handle", "info_sub_id")
+
+  # untested, doesn't appear in browser binary, args unchecked
+  # AcceptSession = svc[0x41].bridge(Types::Result, Types::SessionHandle.pointer, Types::PortHandle).set_names("session_handle", "port_handle")
+
+  # untested, doesn't appear in browser binary, args unchecked
+  # ReplyAndReceive = svc[0x43].bridge(Types::Result, Types::Uint64.pointer, Types::SessionHandle.pointer, Types::Uint64, Types::Uint64, Types::Uint64).set_names("handle_idx", "ptr_handles", "num_handles", "?", "timeout")
+
+  # untested, doesn't appear in browser binary, args unchecked
+  # ReplyAndReceiveByBuf = svc[0x44].bridge(Types::Result, Types::Uint64.pointer, Types::Void.pointer, Types::Uint64, Types::SessionHandle.pointer, Types::Uint64, Types::Uint64, Types::Uint64).set_names("handle_idx", "buf", "sz", "ptr_handles", "num_handles", "?", "timeout")
+
+  # untested, doesn't appear in browser binary, no docs on switchbrew.org
+  # CreateEvent = svc[0x45].bridge( ? )
+  
+  # can't get working - no permissions?
+  # crashes whole console with error code 2168-0003
+  CreateMemoryBlock = svc[0x50].bridge(Types::Result, Types::Handle.pointer, Types::Uint64, Types::Uint64, Types::Uint64).set_names("memBlock", "size", "myPerm", "otherPerm")
+
+  # tested
+  # newly mapped pages have MemoryState 0xE
+  # must pass same size and permissions from CreateMemoryMirror
+  MapMemoryMirror = svc[0x51].bridge(Types::Result, Types::Handle, Types::Void.pointer, Types::Uint64, Types::Uint64).set_names("mirror_handle", "addr", "size", "perm")
+
+  # untested
+  # size must match from MapMemoryMirror, otherwise you get an invalid size error
+  UnmapMemoryMirror = svc[0x52].bridge(Types::Result, Types::Handle.pointer, Types::Void.pointer, Types::Uint64).set_names("mirror_handle", "addr", "size")
 end
 
 class Pointer
